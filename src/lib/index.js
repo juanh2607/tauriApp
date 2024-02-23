0f0b38076b70382d104f91d49ace20f7a2b90da7
juanh2607 <jchernandez@fi.uba.ar>
2024-02-22 22:50:10 -0300

    Wait for DOM Content to load before showing App

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,17 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { appWindow } from '@tauri-apps/api/window';
+
+if (document.readyState === 'complete') {
+  appWindow.show();
+} else { // Document still loading
+  document.addEventListener('DOMContentLoaded', () => {
+    appWindow.show();
+  });
+}
+
+// TODO: eliminar esto
+const testData = {};
+
+// TODO: acá es donde vas a cargar todos los datos que te lleguen del back y vas
+// a crear los componentes (ver como funciona eso con svelte)
+// TODO: crear un timerComponent.svelte
