26c5b62547d857c04e56427b9d265f93ec7f53e5
juanh2607 <jchernandez@fi.uba.ar>
2024-03-06 16:55:27 -0300

    Create (if necessary) temp files when starting app

---
+++ src-tauri/src/file_ops.rs
@@ -0,0 +1,34 @@
+use std::fs;
+use std::io::ErrorKind;
+use std::path::Path;
+
+/// For now I'm checking if the files exists when beginning the program (creating them if necessary).
+/// The rest of the functions assume the files exist and will panic otherwise
+  
+/** Checks if temporary files exist. If not it creates them */
+pub fn check_temp_files(path: &str) {
+    let temp_path = Path::new(path);
+    try_create_dir(&temp_path);
+    try_create_file(&temp_path.join("components_data.json"));
+    try_create_file(&temp_path.join("timer_hist.csv"));
+
+}
+
+fn try_create_dir(path: &Path) {
+    match fs::create_dir(path) {
+        Err(ref e) if e.kind() != ErrorKind::AlreadyExists => {
+            panic!("Couldn't create directory {}: {}", path.display(), e)
+        },
+        _ => () // Created succesfully or already existed
+    }
+}
+
+fn try_create_file(path: &Path) {
+    if !path.exists() {
+        fs::File::create(path).expect(
+            &format!("Couldn't create file {}", path.display())
+        );
+    }
+}
+
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod file_ops;
+
 fn main() {
+  file_ops::check_temp_files("../temp");
+
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
